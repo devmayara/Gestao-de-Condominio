6d17d0810af3825d2016f8f7e0fb09686c5cfcb1
Mayara Silva <devmayara11@gmail.com>
2022-10-13 16:14:47 -0300

    /docs

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doc;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach($docs as $docKey => $docValue) {
+            $docs[$docKey]['fileurl'] = asset('strorage/'.$docValue['fileurl']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
