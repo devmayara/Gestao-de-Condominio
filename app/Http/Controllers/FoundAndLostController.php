e1d97feea9c0f216effd39c73323a4f51cef1e02
Mayara Silva <devmayara11@gmail.com>
2022-10-18 15:51:21 -0300

    /warnings

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+
+
+        return $array;
+
+    }
+
+    public function insert()
+    {
+        $array = ['error' => ''];
+
+
+
+        return $array;
+
+    }
+
+    public function update()
+    {
+        $array = ['error' => ''];
+
+
+
+        return $array;
+
+    }
 }
