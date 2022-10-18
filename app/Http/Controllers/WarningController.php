adc7e33f39e193b43da20979ac147b746b180068
Mayara Silva <devmayara11@gmail.com>
2022-10-18 16:16:46 -0300

    /warning/file

--- app/Http/Controllers/WarningController.php
+++ app/Http/Controllers/WarningController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Unit;
 use App\Models\Warning;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class WarningController extends Controller
 {
@@ -50,23 +52,32 @@ class WarningController extends Controller
         return $array;
     }
 
-    public function setWarning()
+    public function addWarningFile(Request $request)
     {
         $array = ['error' => ''];
 
+        $validator = Validator::make($request->all(), [
+            'photo' => 'required|file|mimes:jpg,png'
+        ]);
 
+        if(!$validator->fails()) {
+            $file = $request->file('photo')->store('public');
+
+            $array['photo'] = asset(Storage::url($file));
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
 
         return $array;
-
     }
 
-    public function addWarningFile()
+    public function setWarning(Request $request)
     {
         $array = ['error' => ''];
 
 
 
         return $array;
-
     }
 }
