ab26b16220d32ad7c0dc7510f52945b25502a5ab
Mayara Silva <devmayara11@gmail.com>
2022-10-20 10:07:36 -0300

    /foundandlost/{id}

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -68,11 +68,24 @@ class FoundAndLostController extends Controller
         return $array;
     }
 
-    public function update()
+    public function update($id, Request $request)
     {
         $array = ['error' => ''];
 
-
+        $status = $request->input('status');
+        if($status && in_array($status, ['lost', 'recovered'])) {
+            $item = FoundAndLost::find($id);
+            if($item) {
+                $item->status = $status;
+                $item->save();
+            } else {
+                $array['error'] = 'Produto inexistente.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Status não existe.';
+            return $array;
+        }
 
         return $array;
     }
